68c74cc7a3c391398ace75c2f61aec77efec5c3d
BriceLucifer <2376671337@qq.com>
2026-06-30 22:47:06 +1200

    Add JSON dependency setup

--- tests/http_dependency_test.cpp
+++ tests/http_dependency_test.cpp
@@ -1,4 +1,5 @@
 #include <httplib.h>
+#include <nlohmann/json.hpp>
 
 #include <gtest/gtest.h>
 
@@ -8,4 +9,11 @@ TEST(HttpDependencyTest, HeaderOnlyLibraryIsAvailable) {
   const auto it = headers.find("Content-Type");
   ASSERT_NE(it, headers.end());
   EXPECT_EQ(it->second, "text/plain");
+}
+
+TEST(JsonDependencyTest, HeaderOnlyLibraryIsAvailable) {
+  const auto body = nlohmann::json{{"name", "minikv"}, {"replicas", 3}};
+
+  EXPECT_EQ(body.at("name"), "minikv");
+  EXPECT_EQ(body.at("replicas"), 3);
 }
